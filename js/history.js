5b2a8ccd1877f53ed8b9824782d7f58cf4b6afd2
George Ngunjiri <ngunjirigeorge995@gmail.com>
2026-05-14 21:08:41 +0300

    feat(history.js):add render history functions for workouts & meals

---
+++ js/history.js
@@ -0,0 +1,51 @@
+// Read all saved data from local storage
+const workouts = JSON.parse(localStorage.getItem('workouts')) || [];
+const meals = JSON.parse(localStorage.getItem('meals')) || [];
+
+// Render functions for history page
+function renderHistory() {
+    // Workouts History
+    const wList = document.getElementById('history-workouts-list');
+    wList.innerHTML = '';
+
+    if (workouts.length === 0) {
+        wList.innerHTML = '<li style="color:#555;">No workouts logged yet.</li>';
+    } else {
+        workouts.forEach((w, i) => {
+            const li = document.createElement('li');
+            li.innerHTML = `
+            <div>
+            <strong>${w.name}</strong> - ${w.duration} mins
+            <span class style="color:#666;">${w.date}</small>
+            </div>
+            <div>
+            <span class="kcal">${w.calories} kcal</span>
+            </span>
+            `;
+            wList.appendChild(li);
+        });
+    }
+
+    // Meals History
+    const Mlist = document.getElementById('history-meal-list');
+    Mlist.innerHTML = '';
+
+    if (meals.length === 0) {
+        Mlist.innerHTML = '<li style="color:#555;">No meals logged yet.</li>';
+    } else {
+        meals.forEach((m, i) => {
+            const li = document.createElement('li');
+            li.innerHTML = `
+            <div>
+            <strong>${m.name}</strong> - ${m.portion}
+            <span class="badge">${m.type}</span>
+            <br/><small style="color:#666;">${m.date}</small>
+            </div>
+            <div>
+            <span class="kcal">${m.calories} kcal</span>
+            </div>
+            `;
+            Mlist.appendChild(li);
+        });
+    }
+}
